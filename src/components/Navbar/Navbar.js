19798bb8b107289b82c863d98224071ab81bf83f
Andrew Bedford-Hart <abedfordhart@gmail.com>
2018-05-14 12:03:32 -0700

    Add initial navbar (rough styling)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {
+  Container,
+  Row,
+  Col,
   Collapse,
   Navbar,
   NavbarToggler,
@@ -11,6 +14,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem } from 'reactstrap';
+import './Navbar.css'
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -29,68 +33,70 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">
-            <img alt="" src="https://d1m7iq40dbyv6q.cloudfront.net/images/logo-secondary.png"/>
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-            <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Explore
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem>
-                    Company
-                  </DropdownItem>
-                  <DropdownItem>
-                    Portfolio
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Press Room
-                  </DropdownItem>
-                  <DropdownItem>
-                    Channel Partners
-                  </DropdownItem>
-                  <DropdownItem>
-                    Industry Council
-                  </DropdownItem>
-                  <DropdownItem>
-                    Blog
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem>
-                <NavLink href="https://www.propelx.com/account/investing">Invest in Startups</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://www.propelx.com/get_listed">List a Startup</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://www.propelx.com/account/expertise">Share Expertise</NavLink>
-              </NavItem>
+        <Container className="propelx-nav" fluid>
+          <Navbar light expand="md">
+            <NavbarBrand href="/">
+              <img alt="" src="https://d1m7iq40dbyv6q.cloudfront.net/images/logo-secondary.png"/>
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  User Profile
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    My Account
-                  </DropdownItem>
-                  <DropdownItem>
-                    Expert Portal
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Logout
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
+                  <DropdownToggle nav caret>
+                    Explore
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Company
+                    </DropdownItem>
+                    <DropdownItem>
+                      Portfolio
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Press Room
+                    </DropdownItem>
+                    <DropdownItem>
+                      Channel Partners
+                    </DropdownItem>
+                    <DropdownItem>
+                      Industry Council
+                    </DropdownItem>
+                    <DropdownItem>
+                      Blog
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <NavItem>
+                  <NavLink href="https://www.propelx.com/account/investing">Invest in Startups</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://www.propelx.com/get_listed">List a Startup</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://www.propelx.com/account/expertise">Share Expertise</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    User Profile
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      My Account
+                    </DropdownItem>
+                    <DropdownItem>
+                      Expert Portal
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Logout
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
       </div>
     );
   }
